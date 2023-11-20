b1a969045b4030c42b2568326dc609bf5e82b41d
mathiasburger <mburakyilmazer38@gmail.com>
2023-11-21 02:03:40 +0300

    stopwatch

--- app.js
+++ app.js
@@ -1,86 +1,104 @@
-const microSeconds = document.querySelector(".microSeconds");
-let seconds = document.querySelector(".seconds");
-let minutes = document.querySelector(".minutes");
-let hours = document.querySelector(".hours");
-const buttons = document.querySelector(".controls");
+document.addEventListener("DOMContentLoaded", function () {
+  const microSeconds = document.querySelector(".microSeconds");
+  const seconds = document.querySelector(".seconds");
+  const minutes = document.querySelector(".minutes");
+  const hours = document.querySelector(".hours");
+  const buttons = document.querySelector(".controls");
+  const listContainer = document.querySelector("section ul");
 
-let count = 0;
-let secondsCount = 0;
-let minutesCount = 0;
-let hoursCount = 0;
-let timer = false;
-let time; // Declare time globally
+  let count = 0;
+  let secondsCount = 0;
+  let minutesCount = 0;
+  let hoursCount = 0;
+  let timer = false;
+  let time; // Declare time globally
 
-function padZero(value) {
-  return value < 10 ? "0" + value : String(value);
-}
+  // Create an object to store the time values
+  const timeData = {
+    count: 0,
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+  };
 
-function padZeroMilliseconds(value) {
-  if (value < 10) {
-    return "00" + value;
-  } else if (value < 100) {
-    return "0" + value;
-  } else {
-    return String(value);
+  function updateTimer() {
+    time = setInterval(() => {
+      if (timer) {
+        timeData.count++;
+
+        if (timeData.count < 100) {
+          microSeconds.innerHTML = padZero(timeData.count);
+        }
+        if (timeData.count >= 100) {
+          timeData.count = 0;
+          timeData.seconds++;
+          if (timeData.seconds < 60) {
+            seconds.innerHTML = padZero(timeData.seconds);
+          }
+          if (timeData.seconds >= 60) {
+            timeData.seconds = 0;
+            seconds.innerHTML = "00";
+            timeData.minutes++;
+            if (timeData.minutes < 60) {
+              minutes.innerHTML = padZero(timeData.minutes);
+            }
+            if (timeData.minutes >= 60) {
+              timeData.minutes = 0;
+              minutes.innerHTML = "00";
+              timeData.hours++;
+              hours.innerHTML = padZero(timeData.hours);
+            }
+          }
+
+          microSeconds.innerHTML = padZero(timeData.count);
+        }
+      }
+    }, 10);
   }
-}
 
-function updateTimer() {
-  time = setInterval(() => {
-    if (timer) {
-      count++;
+  buttons.addEventListener("click", function (event) {
+    clearInterval(time); // Clear the previous interval
 
-      if (count < 100) {
-        microSeconds.innerHTML = padZero(count);
-      }
-      if (count >= 100) {
-        count = 0;
-        secondsCount++;
-        if (secondsCount < 60) {
-          seconds.innerHTML = padZero(secondsCount);
-        }
-        if (secondsCount >= 60) {
-          secondsCount = 0;
-          seconds.innerHTML = "00";
-          minutesCount++;
-          if (minutesCount < 60) {
-            minutes.innerHTML = padZero(minutesCount);
-          }
-          if (minutesCount >= 60) {
-            minutesCount = 0;
-            minutes.innerHTML = "00";
-            hoursCount++;
-            hours.innerHTML = padZero(hoursCount);
-          }
-        }
-
-        microSeconds.innerHTML = padZero(count);
-      }
+    if (event.target.classList.contains("play")) {
+      timer = true;
+      updateTimer();
     }
-  }, 10);
-}
 
-buttons.addEventListener("click", function (event) {
-  clearInterval(time); // Clear the previous interval
+    if (event.target.classList.contains("pause")) {
+      timer = false;
+      // Save the current time data when pause is clicked
+      count = timeData.count;
+      secondsCount = timeData.seconds;
+      minutesCount = timeData.minutes;
+      hoursCount = timeData.hours;
+      listContainer.innerHTML += `<li>${
+        hoursCount < 10 ? "0" + hoursCount : hoursCount
+      } : ${minutesCount < 10 ? "0" + minutesCount : minutesCount} : ${
+        secondsCount < 10 ? "0" + secondsCount : secondsCount
+      } : ${count < 10 ? "0" + count : count} </li> `;
+    }
 
-  if (event.target.classList.contains("play")) {
-    timer = true;
-    updateTimer();
-  }
+    if (event.target.classList.contains("reset")) {
+      timer = false;
+      count = 0;
+      secondsCount = 0;
+      minutesCount = 0;
+      hoursCount = 0;
+      timeData.count = 0;
+      timeData.seconds = 0;
+      timeData.minutes = 0;
+      timeData.hours = 0;
 
-  if (event.target.classList.contains("pause")) {
-    timer = false;
-  }
+      seconds.innerHTML = "00";
+      minutes.innerHTML = "00";
+      hours.innerHTML = "00";
+      microSeconds.innerHTML = "00";
+      listContainer.innerHTML = "";
+    }
+  });
 
-  if (event.target.classList.contains("reset")) {
-    timer = false;
-    count = 0;
-    secondsCount = 0;
-    minutesCount = 0;
-    hoursCount = 0;
-    seconds.innerHTML = "00";
-    minutes.innerHTML = "00";
-    hours.innerHTML = "00";
-    microSeconds.innerHTML = "00";
+  // Function to pad zero as before
+  function padZero(value) {
+    return value < 10 ? "0" + value : String(value);
   }
 });
