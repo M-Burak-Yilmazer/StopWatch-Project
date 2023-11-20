9e1cde8bcddad7e534e23c981c37c5e0f4797145
mathiasburger <mburakyilmazer38@gmail.com>
2023-11-20 16:41:29 +0300

    minutes

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-import lib  from "./lib.js";
-
-
-console.log(lib)
+const seconds = document.querySelector(".minutes");
+let count = 0;
+setInterval(() => {
+  count++;
+  if (count < 60) {seconds.innerHTML = count;}
+  if (count >= 60) {count = 0; minutes.innerHTML = count;}
+}, 10);
